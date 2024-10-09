7ea984d757dbef0f40177856bd9672e87babb87d
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-10-09 20:11:57 +0700

    re render after edit is done, but after data is added still in work

--- assets/javascript/script/crud.js
+++ assets/javascript/script/crud.js
@@ -1,6 +1,12 @@
 let dataGambar = gambar;
 // console.log(dataGambar)
 
+//to obtain data from class
+function getDataFromInput(classInput) {
+    let data = document.getElementById(classInput).value;
+    return data;
+}
+
 //edit array data, by finding editID, and change object.jwb to newJwb
 function edit(data, editID, newJwb){
     let result = [...data] //agar tidak menjadi pass by reference
@@ -10,27 +16,36 @@ function edit(data, editID, newJwb){
         let perGambar = result[x];
 
         //cari yang id sama
-        if(editID === perGambar.id){
+        if(Number(editID) === perGambar.id){
             objTemp = perGambar;
             objTemp.jwb = newJwb;
             result[x] = objTemp
             break;
         }
     }
-
+    // console.log(result)
     return result;
 }
 
+function editButton(editIdClass, newJwbClass){
+    //ambil data
+    let editId = getDataFromInput(editIdClass);
+    let jawaban = getDataFromInput(newJwbClass);
 
+    dataGambar = edit(dataGambar, editId, jawaban);
 
-
-function add() {
-    let jawaban = document.getElementById("jawaban").value;
-    console.log(jawaban);
-    let test = document.getElementsByClassName("test")
-    test.innerText = jawaban;
+    reRender();
+    // console.log(dataGambar)
 }
 
+
+// console.log(dataGambar)
 //nanti edit edit id pake id gambar yang di click, dan newJWB adalzh isi form
 // dataGambar = edit(dataGambar,2, "AAA");
 
+// function button(actionType){
+//     if(actionType === edit){
+
+//     }
+
+// }
--- assets/javascript/script/render.js
+++ assets/javascript/script/render.js
@@ -5,6 +5,7 @@ function render() {
   
   // get new data list
   let imagesData = dataGambar;
+  // console.log(dataGambar);
 
   // put all task to html
   for(let itemImages of imagesData){
@@ -14,19 +15,26 @@ function render() {
     images.classList.add('images')
 
     // create list
-    const id = document.createElement('li')
-    id.innerText = `id: ${itemImages.id}`
-    id.classList.add('image')
-    images.appendChild(id)
-
+    const li = document.createElement('li')
+    li.classList.add('imageBlock')
+    images.appendChild(li)
+    
+    
     //add images
     const displayImage = document.createElement('img')
     displayImage.src = itemImages.url;
     images.appendChild(displayImage)
+    
+    //add id
+    const id = document.createElement('p')
+    id.innerText = `id: ${itemImages.id}`
+    id.classList.add('id')
+    images.appendChild(id)
 
     //add jawaban
     const jawabanImages = document.createElement('p')
     jawabanImages.innerText = `jawaban: ${itemImages.jwb}`;
+    jawabanImages.classList.add('jawabanImage')
     images.appendChild(jawabanImages)
 
     // append to imagesList
@@ -35,3 +43,77 @@ function render() {
 }
 
 render()
+
+
+
+function reRender (){
+    //get child node
+    let target = imagesList.childNodes;
+
+    //get updated data
+    let updatedData = dataGambar;
+
+    // console.log(target[0], "target") //<div class="images">......
+    // console.log(updatedData, "update") //new data
+
+    //cek apakah di update, delete, atau add
+    if (target.length === updatedData.length){
+      //lopping data yang udh di update
+      for(let x = 0; x < updatedData.length; x++){
+        perData = updatedData[x];
+        perTargetNode = target[x];
+
+              //kalo datanya sama berarti update
+        
+        //-------ambil class satu-satu----------------------
+            //ambil id dan update pake updated data
+            let id = perTargetNode.querySelector(".id");
+            id.innerText = `id: ${perData.id}`
+            // console.log(id, perData.id)
+
+            //ambil image
+            let image = perTargetNode.querySelector("img");
+            // console.log(image.innerHTML)
+            image.src = perData.url
+
+            //ambil jawaban
+            let jawabanImages = perTargetNode.querySelector('.jawabanImage')
+            jawabanImages.innerText = `jawaban: ${perData.jwb}`;
+          }
+    } else if(updatedData.length > target.length){
+      //jika data lebih besar, (added), maka bikin baru (samain aja datanya kek render)
+
+      //looping data baru(karena push maka dia mulai dari belakang)
+      for(let init = target.length - 1; x < updatedData.length; x++){
+        let perNewData = updatedData[ini];
+        // create div
+          const images = document.createElement('div')
+          images.classList.add('images')
+
+          // create list
+          const li = document.createElement('li')
+          li.classList.add('imageBlock')
+          images.appendChild(li)
+          
+          //add images
+          const displayImage = document.createElement('img')
+          displayImage.src = perNewData.url;
+          images.appendChild(displayImage)
+          
+          //add id
+          const id = document.createElement('p')
+          id.innerText = `id: ${perNewData.id}`
+          id.classList.add('id')
+          images.appendChild(id)
+
+          //add jawaban
+          const jawabanImages = document.createElement('p')
+          jawabanImages.innerText = `jawaban: ${perNewData.jwb}`;
+          jawabanImages.classList.add('jawabanImage')
+          images.appendChild(jawabanImages)
+
+          // append to imagesList (KARENA imageList adalah sumber awal)
+          imagesList.appendChild(images)
+      }
+    } 
+}
