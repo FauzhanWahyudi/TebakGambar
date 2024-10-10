17d2273dfb4c7a87afa8179ae41b236b4dbc6e6f
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-10-10 18:58:06 +0700

    player sorting

--- assets/javascript/script/point.js
+++ assets/javascript/script/point.js
@@ -1,1 +1,23 @@
-console.log(pemain)
+//ambil nama pemain dan jawabannya
+
+
+function cekJawaban(){
+
+    //ambil data anang
+    for(let perPemain of pemain){
+        //cari data yang jawab
+        let namaPerPemain = perPemain.name
+
+        if(namaPemain == namaPerPemain){
+            // console.log(perPemain);
+            //cek apakah jawabn benar
+            let point = 1000;
+            if(jawabanPemain.toLowerCase() === dataGambar.jwb.toLowerCase()){
+                perPemain.point += point
+            }
+            // console.log(pemain)
+        }  
+    }
+    localStorage.setItem('player', JSON.stringify(pemain));
+    //update data baru dan re render.
+}
