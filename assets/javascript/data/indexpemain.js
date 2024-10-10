42c4e2a0d73403a27bda303f102db05f7920009a
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-10-11 01:25:59 +0700

    game system is finished

--- assets/javascript/data/indexpemain.js
+++ assets/javascript/data/indexpemain.js
@@ -1,29 +1,36 @@
 let pemain = [
     {
+      "index": 0,
       "name": "Belinda Oneal",
       "point": 1091
     },
     {
+      "index": 1,
       "name": "Lora Grant",
       "point": 2762
     },
     {
+      "index": 2,
       "name": "Clara Beach",
       "point": 1551
     },
     {
+      "index": 3,
       "name": "Patti Carr",
       "point": 229
     },
     {
+      "index": 4,
       "name": "Jannie Hendrix",
       "point": 565
     },
     {
+      "index": 5,
       "name": "Jenny Holland",
       "point": 2557
     },
     {
+      "index": 6,
       "name": "Maldonado Patrick",
       "point": 3230
     }
@@ -37,6 +44,8 @@ function sortRank(array){
     return result;
 }
 
+sortRank(pemain)
+
 //RENDER LEADERBOARD ------------------------------------
 let leaderboard = document.querySelector('#leaderboard')
 let storagePemain = JSON.parse(localStorage.getItem('pemain'))
@@ -45,48 +54,63 @@ function renderLeaderboard(array) {
     leaderboard.innerHTML = "";
     for (let x = 0; x < array.length; x++){
         leaderboard.innerHTML +=`
-        <div class="rank">
-            <li class="rank-item"> 
-                    Rank ${x + 1} : ${array[x].name}
+            <li> 
+                <div>
+                    Rank ${x + 1} : ${array[x].name} with ${array[x].point}
+                </div>
+                <div>
+                    point: ${array[x].point}
+                </div>
             </li>
-            <button type="button" class="point"> point: ${array[x].point} </button>
-        </div>
         `
     }
 }
 
 if(storagePemain === null){
-    sortRank(pemain)
     renderLeaderboard(pemain)
-    renderNextImage(0);
 } else {
     renderLeaderboard(storagePemain);
-    renderNextImage(0);
 }
 
 //RENDER IMAGE FOR PLAY ------------------------------------
+let displayImage = document.querySelector('.cards')
 function renderNextImage(id) {
-    let displayImage = document.querySelector('.cards')
+    // Konversi id saat ini ke angka, dan hitung id berikutnya
     let nextId = Number(id) + 1;
-    let storageImage = JSON.parse(localStorage.getItem('gambar'))
+    
+    // Ambil elemen tempat gambar akan ditampilkan
+    let displayImage = document.querySelector('.cards');
+    
+    // Ambil data gambar dari localStorage
+    let storageImage = JSON.parse(localStorage.getItem('gambar'));
+    
+    // Bersihkan konten yang ada
     displayImage.innerHTML = "";
-    if(nextId > storageImage.length){
-        displayImage.innerHTML +=`<h1 id="finished">
-        <a href="./page1.html" style="
-  text-decoration: none;">FINISHED</a>
-        </h1>`
-        return 0;
-    }
-    for (let x = 0; x < storageImage.length; x++){
-        //cari id
-        if(storageImage[x].id === nextId){
-            displayImage.innerHTML +=`
-                <div class="card">
-                    <!-- masukkan foto gambar yang sudah dimunculkan -->
+
+    // Cari gambar dengan id berikutnya di array
+    let foundImage = false;
+    for (let x = 0; x < storageImage.length; x++) {
+        // Jika id gambar sesuai dengan id berikutnya
+        if (storageImage[x].id === nextId) {
+            displayImage.innerHTML = `
+                <div style="margin: 0 10px;">
+                    <!-- Masukkan gambar yang ditemukan -->
                     <img src="${storageImage[x].url}" alt="${storageImage[x].id}">
                 </div>
-            `
+            `;
+            foundImage = true; // Tandai gambar ditemukan
+            break; // Keluar dari loop karena gambar sudah ditemukan
         }
+    }
+
+    // Jika tidak ditemukan, reset ke gambar pertama
+    if (!foundImage) {
+        displayImage.innerHTML = `
+            <div style="margin: 0 10px;">
+                <!-- Jika tidak ada gambar berikutnya, tampilkan gambar pertama -->
+                <img src="${storageImage[0].src}" alt="${storageImage[0].id}">
+            </div>
+        `;
     }
 }
 
@@ -109,13 +133,12 @@ function clickDone(){
             break;
         }
     }
-    if (imageJwb.toLowerCase() !== isiJawabanUser.toLowerCase()){
+    if (imageJwb !== isiJawabanUser){
         //kalo salah
         jawabanUser.value = "SALAH !!! Coba Lagi"
     } else {
-        //kalo benar
         jawabanUser.value = "BENAR !!! Good Job"
-        let pemain = document.querySelector('#greeting')
+        let pemain = document.querySelector('h2')
         let namaPemain = pemain.title;
         // console.log(pemain.title) //akbar
 
@@ -124,41 +147,11 @@ function clickDone(){
         let storagePemain = JSON.parse(localStorage.getItem('pemain'))
         let pemainIndex = storagePemain.findIndex(obj => obj.name == namaPemain);
         storagePemain[pemainIndex].point += 1000;
-        localStorage.setItem('pemain', JSON.stringify(storagePemain));
-        pemain = [...storagePemain];
-
-        //update leaderboard
+        
+        sortRank(storagePemain);
         renderLeaderboard(sortRank(storagePemain));
-
-        
-        let doneButton = document.getElementById('doneButton')
-        let lanjutButton = document.getElementById('lanjutButton')
-        
-        doneButton.style.display = "none";
-        lanjutButton.style.display = "inline";
     }
     // console.log(pemain) //IOT aaa
-}
-
-function lanjut(){
-    
-    let image = document.querySelector('.cards div img')
-    let id = image.alt;
     renderNextImage(id)
-
-    let doneButton = document.getElementById('doneButton')
-    let lanjutButton = document.getElementById('lanjutButton')
-    let jawabanUser = document.querySelector('#isiJwb');
-
-    jawabanUser.value = ""
-    jawabanUser.placeholder="Isi disini..."
-
-    doneButton.style.display = "inline";
-    lanjutButton.style.display = "none";
-}
-
-function navigationBar(){
-    const Bar = document.querySelector('.navigationBar');
-    Bar.classList.toggle('hidup');
 
 }
